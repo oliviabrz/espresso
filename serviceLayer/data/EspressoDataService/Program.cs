7b9551b9ffbc975cd38b12b556b59745a3270bd9
oliviabrz@gmail.com <oliviabrz@gmail.com>
2024-09-25 20:21:09 -0500

    swagger configuration!

--- serviceLayer/data/EspressoDataService/Program.cs
+++ serviceLayer/data/EspressoDataService/Program.cs
@@ -1,13 +1,28 @@
-using System.ComponentModel.DataAnnotations;
 using MySqlConnector;
 using Dapper;
 using EspressoDataService.Dto;
+using Microsoft.OpenApi.Models;
 
+// Create a new WebApplication
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the application
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Espresso API", Version = "v1" });
+});
+
+// Configure MySQL connection
+builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(builder.Configuration.GetConnectionString("ConnectionStrings.EspressoDatabase")));
+
+// Build the application
 var app = builder.Build();
 
-// CREATE Grinder
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Espresso API v1"));
+
+// CREATE Grinder API Route
 app.MapPost("/grinder", async (GrinderDto grinder, MySqlConnection connection) =>
 {
     var sql = "INSERT INTO Grinder (BrandName, Model) VALUES (@BrandName, @Model);";
@@ -15,11 +30,11 @@ app.MapPost("/grinder", async (GrinderDto grinder, MySqlConnection connection) =
     return Results.Created($"/grinder/{grinder.Id}", grinder);
 });
 
-// READ Grinder
+// READ Grinder API Route
 
-// UPDATE Grinder
+// UPDATE Grinder API Route
 
-// DELETE Grinder
+// DELETE Grinder API Route
 
 
 app.Run();

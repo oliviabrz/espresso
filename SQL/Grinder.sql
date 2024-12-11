4881e1d0a1988fa66ab55eb62c943c93b7356cef
oliviabrz@gmail.com <oliviabrz@gmail.com>
2024-12-10 19:45:16 -0600

    modified grinder sql statement

--- SQL/Grinder.sql
+++ SQL/Grinder.sql
@@ -3,5 +3,6 @@
 CREATE TABLE `Grinder` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `BrandName` varchar(100) NOT NULL,
+  `Model` varchar(75) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

0941fbefa095beb0f64b1d70ef41e5f906acb53f
oliviabrz@gmail.com <oliviabrz@gmail.com>
2024-12-16 18:18:32 -0600

    fixed db error

--- serviceLayer/data/EspressoDataService/Api/GrinderApi.cs
+++ serviceLayer/data/EspressoDataService/Api/GrinderApi.cs
@@ -14,8 +14,8 @@ public static class GrinderApi
         app.MapPost("/grinder", async (GrinderDtoBase grinder, MySqlConnection connection) =>
         {
             var sql = @"
-                INSERT INTO Grinder (BrandName, Model) 
-                VALUES (@BrandName, @Model);
+                INSERT INTO Grinder (BrandName, ModelName) 
+                VALUES (@BrandName, @ModelName);
                 SELECT LAST_INSERT_ID();";
             var id = await connection.ExecuteScalarAsync<int>(sql, grinder);
             return Results.Created($"/grinder/{id}", id);
@@ -44,7 +44,7 @@ public static class GrinderApi
         {
             var sql = @"
                 UPDATE Grinder 
-                SET BrandName = @BrandName, Model = @Model
+                SET BrandName = @BrandName, Model = @ModelName
                 WHERE Id = @id";
             var rowsAffected = await connection.ExecuteAsync(sql, new { id, grinder.BrandName, grinder.ModelName });
             return rowsAffected == 0 ? Results.NotFound() : Results.NoContent();

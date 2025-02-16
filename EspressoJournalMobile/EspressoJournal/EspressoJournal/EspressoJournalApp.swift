eb4ac51700dcb8b60e94b5eaabb21fe6a0842464
oliviabrz@gmail.com <oliviabrz@gmail.com>
2025-02-16 15:50:54 -0600

    created swift project

---
+++ EspressoJournalMobile/EspressoJournal/EspressoJournal/EspressoJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EspressoJournalApp.swift
+//  EspressoJournal
+//
+//  Created by Olivia Brzozowski on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EspressoJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EspressoJournalMobile/EspressoJournal/EspressoJournalTests/EspressoJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  EspressoJournalTests.swift
+//  EspressoJournalTests
+//
+//  Created by Olivia Brzozowski on 2/12/25.
+//
+
+import Testing
+@testable import EspressoJournal
+
+struct EspressoJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
